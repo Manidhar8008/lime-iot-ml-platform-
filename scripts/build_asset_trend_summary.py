8d34f2cf129893c2a87b612521d97fef18f51a05
Manidhar Pati <manidharmanidhar2@gmail.com>
2025-12-30 22:39:51 +0530

    Update build_asset_trend_summary.py

--- scripts/build_asset_trend_summary.py
+++ scripts/build_asset_trend_summary.py
@@ -1,1 +1,15 @@
+"""
+Purpose:
+    Generate aggregated trend summaries from proxy features
+
+Input:
+    data/generated/asset_proxy_features_v1.csv
+
+Output:
+    data/generated/asset_trend_summary_v1.csv
+
+Audience:
+    - Leadership
+    - Strategy
+"""
 
